7709bc79dea06c6fb2bb696188310c3df8e7fa1a
Amos Chan <amosschan@gmail.com>
2016-11-03 19:15:03 -0400

    Set up specs with Quick / Nimble

---
+++ FisherHallClientTests/Resources/GroupResourceSpec.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Quick
+import Nimble
+
+class GroupResourceSpec: QuickSpec {
+	override func spec() {
+		describe("it works") {
+			it("works") {
+				expect(3).to(equal(2 + 1))
+			}
+		}
+	}
+}
